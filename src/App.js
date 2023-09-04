bd57300d603dd41867653874b0e3446907e487e6
RomJ25 <romjacobjan25@gmail.com>
2023-09-04 09:15:24 +0300

    added input

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 import { randomFunc } from './algo';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>{randomFunc(5, 0)}</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random Number Generator!</h1>
+      <form>
+        <label>
+          <input type="text" name="Max" placeholder="Max" />
+        </label>
+        <label>
+          <input type="text" name="Min" placeholder="Min" />
+        </label>
+        <input type="submit" value="Generate" />
+      </form>
     </div>
   );
 }
--- src/algo.js
+++ src/algo.js
@@ -1,3 +1,6 @@
 export function randomFunc(max, min) {
     return Math.round(Math.random() * (max - min) - min)
 }
+
+
+
